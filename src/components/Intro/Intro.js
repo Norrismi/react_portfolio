78abeddee57a916f319ca3eb0a2f091266b96aa8
Norrismi <Norrismi0@gmail.com>
2021-02-11 15:17:39 -0500

    added profile links

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,14 +1,20 @@
 import React from "react";
 import "./Intro.css";
 import MDNpicture from "../../assets/MDN.jpg";
+import { Link } from 'react-router-dom'
+import {
+  FaGithub,
+  FaCode,
+  FaLinkedin
 
+} from "react-icons/fa";
 
 const Intro = () => {
   return (
     <div className="intro__container">
       <div>
         <img className='intro__img' src={MDNpicture} alt="author" />
-     
+
       </div>
       <div className="intro__text-links">
         <div className="intro__name">Michael Norris</div>
@@ -17,7 +23,7 @@ const Intro = () => {
           href="mailto:Norrismi0@gmail.com"
           target="_blank"
           rel="noopener noreferrer"
-          id = 'intro__link'
+          id='intro__link'
         >
           Norrismi0@gmail.com
         </a>
@@ -25,14 +31,54 @@ const Intro = () => {
           I'm currently seeking opportunities to work, grow and help the
           development community.
         </div>
-        <a
-          href="https://github.com/Norrismi"
-          target="_blank"
-          rel="noopener noreferrer"
-          id = 'intro__link'
-        >
-          Github.com/Norrismi
-        </a>
+
+        <div className='intro__icon-container'>
+          <div className='intro__github'>
+            <a
+              href="https://github.com/Norrismi"
+              target="_blank"
+              rel="noopener noreferrer"
+              alt='GitHub'
+              title='GitHub'
+            >
+              <FaGithub id='intro__icon' />
+            </a>
+          </div>
+
+
+          <div className='intro__linkedin'>
+            <a
+              href="https://bit.ly/3q8QbAG"
+              target="_blank"
+              rel="noopener noreferrer"
+              alt='LinkedIn'
+              title='LinkedIn'
+            >
+
+              <FaLinkedin id='intro__icon' />
+              <span title='LinkedIn' ></span>
+            </a>
+          </div>
+          <div className='intro__codewars'>
+            <a
+              href="https://www.codewars.com/users/Norrismi"
+              target="_blank"
+              rel="noopener noreferrer"
+              alt='CodeWars'
+              title='CodeWars Profile'
+            >
+              <FaCode id='intro__icon' />
+            </a>
+          </div>
+
+
+
+
+
+        </div>
+
+
+
 
       </div>
     </div>
